3436a3b70642c61dd3366ae0c6e3653e25ccf1b7
Alex <alex_sobko.94@mail.ru>
2016-09-12 18:30:27 +0300

    Добавлен подсчет узлов интерполирования.

--- data.js
+++ data.js
@@ -13,3 +13,30 @@ var a = 1.1, b = 5;
 
 // количество отрезков, на которое будет разбито [a, b]
 var n = 7;
+
+// длинна каждого отрезка
+var h = (b - a) / n;
+
+// узлы интерполирования
+var interpolationPoints = [];
+
+// подсчет узлов интерполирования
+(function() {
+    // хранит абсциссу каждой подсчитываемой точки
+    var tmpX = a;
+
+    interpolationPoints[0] = [];
+    interpolationPoints[0][0] = a;
+    interpolationPoints[0][1] = f(a);
+
+    interpolationPoints[7] = [];
+    interpolationPoints[7][0] = a;
+    interpolationPoints[7][1] = f(a);
+
+    for (var i = 1; i < 7; i++) {
+        tmpX += h;
+        interpolationPoints[i] = [];
+        interpolationPoints[i][0] = tmpX;
+        interpolationPoints[i][1] = f(tmpX);
+    }
+})();
