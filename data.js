74202f40c26a9ce6578f1da542567d0f7e1bb56c
Alex <alex_sobko.94@mail.ru>
2016-09-15 18:44:06 +0300

    Добавлен класс, содержащий базовые данные.

--- data.js
+++ data.js
@@ -1,42 +1,44 @@
-// заданная функция, для которой необходимо найти приближение
-function f(x) {
-    return Math.exp(x/2) + 3.5 * cosh(x/2);
-}
+// класс, описывающий заданные данные
+function BasicData() {
 
-// косинус гиперболический
-function cosh(x) {
-    return (Math.exp(x) + Math.exp(-x)) / 2;
-}
-
-// отрезок, на котором необходимо найти приближение
-var a = 1.1, b = 5;
-
-// количество отрезков, на которое будет разбито [a, b]
-var n = 7;
-
-// длинна каждого отрезка
-var h = (b - a) / n;
-
-// узлы интерполирования
-var interpolationPoints = [];
-
-// подсчет узлов интерполирования
-(function() {
-    // хранит абсциссу каждой подсчитываемой точки
-    var tmpX = a;
-
-    interpolationPoints[0] = [];
-    interpolationPoints[0][0] = a;
-    interpolationPoints[0][1] = f(a);
-
-    interpolationPoints[7] = [];
-    interpolationPoints[7][0] = a;
-    interpolationPoints[7][1] = f(a);
-
-    for (var i = 1; i < 7; i++) {
-        tmpX += h;
-        interpolationPoints[i] = [];
-        interpolationPoints[i][0] = tmpX;
-        interpolationPoints[i][1] = f(tmpX);
+    // косинус гиперболический
+    function cosh(x) {
+        return (Math.exp(x) + Math.exp(-x)) / 2;
     }
-})();
+
+    // отрезок, на котором необходимо найти приближение
+    this.a = 1.1, this.b = 5;
+
+    // количество отрезков, на которое будет разбито [a, b]
+    this.n = 7;
+
+    // длинна каждого отрезка
+    this.h = (this.b - this.a) / this.n;
+
+    // узлы интерполирования
+    this.points = [];
+
+
+    // подсчет узлов интерполирования, tmpX - хранит абсциссу каждой подсчитываемой точки
+    var tmpX = this.a;
+
+    this.points[0] = [];
+    this.points[0][0] = this.a;
+    this.points[0][1] = this.f(this.a);
+
+    this.points[this.n] = [];
+    this.points[this.n][0] = this.a;
+    this.points[this.n][1] = this.f(this.a);
+
+    for (var i = 1; i < this.n; i++) {
+        tmpX += this.h;
+        this.points[i] = [];
+        this.points[i][0] = tmpX;
+        this.points[i][1] = this.f(tmpX);
+    }
+}
+
+// заданная функция, для которой необходимо найти приближение
+BasicData.prototype.f = function(x) {
+    return Math.exp(x / 2) + 3.5 * cosh(x / 2);
+}
